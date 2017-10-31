a7c46edb05318427f779c21c8cbce245c4431967
Sheldon Nunes <sheldon.nunes02@gmail.com>
2017-10-31 22:19:25 +1300

    Fixing bug with wrong brackets being used on plotDataSet function

--- Introduction/Classifying iris species/classifying_iris_species.py
+++ Introduction/Classifying iris species/classifying_iris_species.py
@@ -6,18 +6,16 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-def plotDataSet()
-{
-        iris_dataset = load_iris()
-        X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
-
+def plotDataSet():
         # create dataframe from data in X_train
         # label the columns using the string in iris_dataset.feature_names
         iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
         # create a scatter matrix from the dataframe, colour by y_train
         ts = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15,15), marker='o', hist_kwds={'bins' : 20}, s=60, alpha=.8, cmap=mglearn.cm3)
         plt.show()
-}
+
+iris_dataset = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 
 plotDataSet()
 
